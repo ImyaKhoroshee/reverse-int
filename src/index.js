894c31c9d489afc896b16aa12843151a80048364
tatyana <tilkka@mail.ru>
2023-01-04 18:07:48 +0300

    feat: add my solution of reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    n = Math.abs(n);
+    let result = "";
+    while (n > 9) {
+        result += String(n % 10);
+        n = Math.trunc(n / 10);
+    }
+    result += n;
+    return result;
 }
